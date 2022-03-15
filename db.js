f938f2505e96d7578d5c1fca33978c6c965c98cc
Cristian Franco <cfrancobedoya@gmail.com>
2022-03-14 22:16:01 -0500

    Add main structure

---
+++ db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+async function connect(url) {
+    // mongoose.set('useCreateIndex', true);
+    await mongoose.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    console.log('[db] succesfully connected');
+}
+
+module.exports = connect;
